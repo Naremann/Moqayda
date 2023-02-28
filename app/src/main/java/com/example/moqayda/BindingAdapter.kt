144da2769cb97d4ea96debc61ceec00a34edff59
AbdoTalaat74 <57468506+AbdoTalaat74@users.noreply.github.com>
2023-02-28 21:22:30 +0200

    Revert "Development"

--- app/src/main/java/com/example/moqayda/BindingAdapter.kt
+++ app/src/main/java/com/example/moqayda/BindingAdapter.kt
@@ -1,22 +1,9 @@
 package com.example.moqayda
 
-import android.widget.ImageView
-import androidx.core.net.toUri
 import androidx.databinding.BindingAdapter
-import androidx.recyclerview.widget.RecyclerView
-import com.example.moqayda.models.CategoryItem
-import com.example.moqayda.ui.home.CategoryItemAdapter
 import com.google.android.material.textfield.TextInputLayout
-import com.squareup.picasso.Picasso
 
 @BindingAdapter("layout_error")
 fun setError(inputLayout: TextInputLayout, error:String?){
     inputLayout.error = error
-}
-@BindingAdapter("photoUrl")
-fun bindImage(imageView: ImageView, url: String?) {
-    url?.let {
-        val photoUri = url.toUri().buildUpon().scheme("http").build()
-        Picasso.with(imageView.context).load(photoUri).into(imageView)
-    }
 }
--- app/src/main/java/com/example/moqayda/ui/home/Navigator.kt
+++
@@ -1,7 +0,0 @@
-package com.example.moqayda.ui.home
-
-import com.example.moqayda.models.CategoryItem
-
-interface Navigator {
-    fun navigateToProductListFragment(categoryItem: CategoryItem)
-}
