63f69b49484e4a2934a5fc28ea83a0e9d4924b51
Naremann <naremansharkan5@gmail.com>
2023-03-06 21:24:11 +0200

    create product screen fetch product data(get products data by category id)

--- app/src/main/java/com/example/moqayda/api/ApiInterface.kt
+++ app/src/main/java/com/example/moqayda/api/ApiInterface.kt
@@ -1,22 +1,16 @@
 package com.example.moqayda.api
 
-import com.example.moqayda.models.Category
-import com.example.moqayda.models.CategoryItem
-import com.example.moqayda.models.CategoryResponse
-import com.example.moqayda.models.Product
+import com.example.moqayda.models.*
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface ApiInterface {
 
     @GET("/api/Category")
     suspend fun getAllCategories(): Response<CategoryResponse>
 
-    @GET("/api/Category")
-    suspend fun getCategories(): Category
-
     @GET("/api/Product")
-    suspend fun getProducts(): Product
-
+    suspend fun getProductsByCategoryId(@Query("categoryId") categoryId:Int?): Response<ProductResponse>
 
 }
