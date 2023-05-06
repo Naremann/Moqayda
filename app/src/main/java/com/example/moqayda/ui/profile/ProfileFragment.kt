8bc5adc72313ab242566f4fc84921b69870ed8cb
Naremann <naremansharkan5@gmail.com>
2023-05-07 00:10:01 +0200

    view receiver image on chat recycler

--- app/src/main/java/com/example/moqayda/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/moqayda/ui/profile/ProfileFragment.kt
@@ -33,7 +33,7 @@ class ProfileFragment : BaseFragment<FragmentProfileBinding,ProfileViewModel>() 
     }
 
     private fun loadUserImage(){
-        viewDataBinding.progressBar.isVisible=false
+        viewDataBinding.progressBar.isVisible=true
         DataUtils.USER?.id?.let { userId ->
             getUerImageFromFirebase(object: ValueEventListener {
                 override fun onDataChange(snapshot: DataSnapshot) {
