7988a7ea66846c14e039ff9c46c82373118d3508
Naremann <naremansharkan5@gmail.com>
2023-06-07 15:52:24 +0200

    unit testing-> updateItemWithEmptyFieldsReturnErrorMessage()

--- app/src/androidTest/java/com/example/moqayda/ui/updtaeProduct/UpdateProductViewModel.kt
+++ app/src/androidTest/java/com/example/moqayda/ui/updtaeProduct/UpdateProductViewModel.kt
@@ -4,20 +4,36 @@ import android.content.Context
 import android.net.Uri
 import com.example.moqayda.R
 import com.example.moqayda.getOrAwaitValue
-import kotlinx.coroutines.test.runBlockingTest
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.SmallTest
+import com.example.moqayda.MainCoroutineRule
 import org.junit.Assert
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import com.example.moqayda.repo.product.ProductRepository
+import com.example.moqayda.repo.product.Resource
 import com.example.moqayda.ui.updateProduct.UpdateProductViewModel
+import com.google.common.truth.Truth.assertThat
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.runner.RunWith
 
+@ExperimentalCoroutinesApi
+@RunWith(AndroidJUnit4::class)
+@SmallTest
 class UpdateProductViewModel {
     @get:Rule
     var instantTaskExecutorRule = InstantTaskExecutorRule()
-    lateinit var repo: ProductRepository
+
+    @get:Rule
+    var mainCoroutineRule = MainCoroutineRule()
+
+
+    private lateinit var repo: ProductRepository
 
     private val context : Context = ApplicationProvider.getApplicationContext()
     lateinit var viewModel : UpdateProductViewModel
@@ -30,10 +46,14 @@ class UpdateProductViewModel {
 
 
     @Test
-    fun updateItemWithEmptyFieldsReturnErrorMessage() = runBlockingTest{
+    fun updateItemWithEmptyFieldsReturnErrorMessage() = runBlocking{
+
+        //content://com.google.android.apps.photos.contentprovider/-1/1/content%3A%2F%2Fmedia%2Fexternal%2Fimages%2Fmedia%2F31/ORIGINAL/NONE/1616429549
+
         repo.updateProduct("id","","desc","false","1",
-            "0","a", Uri.parse("https://image.tmdb.org/t/p/w92/dRLSoufWtc16F5fliK4ECIVs56p.jpg"))
-        val value = viewModel.messageLiveData.getOrAwaitValue()
+            "0","a", Uri.parse("android.resource://${context.packageName}/" + R.drawable.background))
+        val value = viewModel.toastMessage.getOrAwaitValue()
+
         Assert.assertEquals(context.getString(R.string.fill_all_fields),value)
     }
 
